fee8fc3f16e3e6eee06afb8ba6fa4dbe8d68cbb2
alxwnth <alxwnth@aol.com>
2023-01-13 23:49:16 +0200

    feat: Keywords and description

--- src/components/MetaHead.svelte
+++ src/components/MetaHead.svelte
@@ -1,6 +1,12 @@
 <svelte:head>
 	<meta charset="utf-8" />
 	<title>Pass2Heaven - Digital online inheritance</title>
+	<meta name="keywords" content="inheritance, privacy, blockchain" />
+	<meta
+		name="description"
+		content="Pass2Heaven is an inheritance protocol for sending information directly to the recipient while
+    ensuring the data's integrity and confidentiality with verifiable ownership."
+	/>
 	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
 	<link rel="manifest" href="site.webmanifest" />
 </svelte:head>
