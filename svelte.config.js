03346c12f8e204d6b11f6f5656716a3f7b9b97a6
alxwnth <alxwnth@aol.com>
2023-01-13 23:55:50 +0200

    feat: Add DigitalOcean adapter for static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-static-digitalocean';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			spec: '.do/spec.yaml',
+			name: ''
+		})
+	},
 };
 
 export default config;
