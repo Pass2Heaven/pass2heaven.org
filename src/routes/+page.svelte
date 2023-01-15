bf995f5ce552a13bf90132389d3510928f37c294
alxwnth <alxwnth@aol.com>
2023-01-15 13:49:16 +0200

    feat: Add events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,10 @@
 
 <div class="row flex-center" style="width: 40%; text-align: center">
 	<div class="sm-4 col">
-		<a href="https://app.pass2heaven.org"><Button type="secondary">Try it Now!</Button></a>
+		<a href="https://app.pass2heaven.org"><Button type="secondary" class="umami--click--try-button">Try it Now!</Button></a>
 	</div>
 	<div class="sm-4 col align-middle">
-		<a href="/" on:click={() => (showModal = true)}>Download Whitepaper</a>
+		<a href="/" on:click={() => (showModal = true)} class="umami--click--download-whitepaper-modal">Download Whitepaper</a>
 	</div>
 </div>
 
