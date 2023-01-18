00e45de84c6c59b494929fa6db732e7253b45910
alxwnth <alxwnth@aol.com>
2023-01-18 17:59:08 +0200

    fix: Fucking umami breaks the modal for some reason so fuck it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,14 @@
 
 <div class="row flex-center" style="width: 40%; text-align: center">
 	<div class="sm-4 col">
-		<a href="https://app.pass2heaven.org" class="umami--click--try-button"><Button type="secondary">Try it Now!</Button></a>
+		<a href="https://app.pass2heaven.org" class="umami--click--try-button"
+			><Button type="secondary">Try it Now!</Button></a
+		>
 	</div>
 	<div class="sm-4 col align-middle">
-		<a href="/" on:click={() => (showModal = true)} class="umami--click--download-whitepaper-modal">Download Whitepaper</a>
+		<a href="/" on:click={() => (showModal = true)}
+			>Download Whitepaper</a
+		>
 	</div>
 </div>
 
